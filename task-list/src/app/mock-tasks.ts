5a2b3f42ab196f8f210029b938d6e2647c9e4264
lucialeitessanchez <lucialeitessanchez@gmail.com>
2023-04-28 13:27:52 -0300

    haciendo el mock de task

---
+++ task-list/src/app/mock-tasks.ts
@@ -0,0 +1,20 @@
+export const TASK = [
+    {
+        id: 1,
+        text: "Terminar primer modulo de Angular",
+        day: 'Agosto 5 a las 12:00',
+        reminder: true
+    },
+    {
+        id: 2,
+        text: "Hacer las compras para la cena",
+        day: 'Agosto 5 a las 17:00',
+        reminder: true
+    },
+    {
+        id: 3,
+        text: "Empezar con los ejercicios de bicicleta",
+        day: 'Agosto 5 a las 12:00',
+        reminder: true
+    }
+]
