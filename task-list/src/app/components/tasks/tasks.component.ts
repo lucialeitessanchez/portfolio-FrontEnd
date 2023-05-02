7ba2177ee7278956202ab51e64689afc85cb0395
lucialeitessanchez <lucialeitessanchez@gmail.com>
2023-05-02 14:15:42 -0300

    task service

--- task-list/src/app/components/tasks/tasks.component.ts
+++ task-list/src/app/components/tasks/tasks.component.ts
@@ -1,12 +1,24 @@
-import { Component } from '@angular/core';
-import { Task} from '../../Task';
-import {TASKS} from '../../mock-tasks'
+import { Component, OnInit } from '@angular/core';
+import { TaskService } from '../../service/task.service';
+import { Task } from '../../Task';
+
 
 @Component({
   selector: 'app-tasks',
   templateUrl: './tasks.component.html',
   styleUrls: ['./tasks.component.css']
 })
-export class TasksComponent {
-  tasks: Task[] = TASKS;
-}
+
+export class TasksComponent implements OnInit {
+  tasks: Task[] = [];
+
+  constructor(
+    private taskService: TaskService
+    ) {}
+
+  ngOnInit(): void {
+    this.taskService.getTask().subscribe((tasks) => {
+      this.tasks = tasks
+    });
+  }
+} 
