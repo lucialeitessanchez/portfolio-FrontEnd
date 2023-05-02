c86c582caef97f6b64c7682f964af7b76cf7a95c
lucialeitessanchez <lucialeitessanchez@gmail.com>
2023-05-02 11:03:02 -0300

    componente de la lista items

--- task-list/src/app/components/tasks-item/tasks-item.component.ts
+++ task-list/src/app/components/tasks-item/tasks-item.component.ts
@@ -1,7 +1,7 @@
 import { Component,Input } from '@angular/core';
 import { TASKS } from 'src/app/mock-tasks';
 import { Task } from '../../Task';
-
+import { faTimes } from '@fortawesome/free-solid-svg-icons';
 @Component({
   selector: 'app-tasks-item',
   templateUrl: './tasks-item.component.html',
@@ -9,4 +9,5 @@ import { Task } from '../../Task';
 })
 export class TasksItemComponent {
   @Input() task: Task = TASKS[0]
+  faTimes = faTimes;
 }
